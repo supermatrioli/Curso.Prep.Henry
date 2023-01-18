b0b48002bf6d31ca82f2acc5fe80836190299789
supermatrioli <elinjojisu@gmail.com>
2023-01-17 23:09:02 -0300

    homework 1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,14 +1,14 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = hola
+const nuevaString = "Hola"
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 1233555;
 
 // Crea una variable booleana:
-const nuevoBool = var estoyaprendiendo = true;
- var estoyaburrido = false; 
+const nuevoBool = true;
+
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
 
@@ -16,7 +16,7 @@ const nuevaResta = 10 - 5 === 5;
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === 4.2;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
---
+++ 02-JS-I/homework/instrucciones.js
@@ -0,0 +1,3 @@
+" una variables es similar a un cajon que contiene informacion "
+"puede ser del tipo  strings que posee comillas al principio y final del texto"
+" tambien booleanos" " numeros" " "
